4babf0e5c0c8503f7ebc11cd0e1fdaeecb629d83
Howard Butler <howard@hobu.co>
2020-12-12 12:24:42 -0600

    noise

--- scripts/ci/shell/exit-early.sh
+++ scripts/ci/shell/exit-early.sh
@@ -6,5 +6,18 @@ then
     exit 0;
 fi
 
+CI_PLAT=""
+if [ "$PLATFORM" == "windows-latest" ]; then
+    CI_PLAT="win"
+fi
 
+if [ "$PLATFORM" == "ubuntu-latest" ]; then
+    CI_PLAT="linux"
+fi
+
+if [ "$PLATFORM" == "macos-latest" ]; then
+    CI_PLAT="osx"
+fi
+
+ls
 echo "Printing some other stuff"
