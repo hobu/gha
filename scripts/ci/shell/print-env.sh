5491ba73e15afa18c9555faf354c887210c1ee0b
Howard Butler <howard@hobu.co>
2020-12-14 16:28:16 -0600

    variable expansion

--- scripts/ci/shell/print-env.sh
+++ scripts/ci/shell/print-env.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+
 keyname="$1"
-if [ -z "${keyame+x}" ]
+if [ -z "${!keyame+x}" ]
 then
-    echo "$keyame was not available in the environment"
+    echo "${!keyame} was not available in the environment"
     echo "Exiting early"
     exit 0;
 fi
 
 
-if [ -z "${keyname}" ]
+if [ -z "${!keyname}" ]
 then
-    echo "$keyname was set to empty"
+    echo "${!keyname} was set to empty"
     echo "Exiting early"
     exit 0;
 fi
 
-echo "$keyname is set and available!"
+echo "${!keyname} is set and available!"
